7504c9a8ac332e47b1bee61faeee80461edf99d9
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-07-16 16:27:10 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import user from './DataJSON/user.json';
 import data from './DataJSON/data.json';
-import friends from './DataJSON/friends';
+// import friends from './DataJSON/friends';
 
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
+// import FriendList from './FriendList/FriendList';
 
 export const App = () => {
   return (
